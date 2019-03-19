252baad387b764c85fdbbae2bc884f934dc91ff6
nikaku <qurdadzenika@gmail.com>
2019-03-19 18:38:22 +0400

    first

---
+++ Form1.b1f.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using SAPbobsCOM;
+using SAPbouiCOM.Framework;
+
+namespace CostCenterOutgoing
+{
+    [FormAttribute("CostCenterOutgoing.Form1", "Form1.b1f")]
+    class Form1 : UserFormBase
+    {
+        public Form1()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_0").Specific));
+            this.Button0.PressedAfter += new SAPbouiCOM._IButtonEvents_PressedAfterEventHandler(this.Button0_PressedAfter);
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private SAPbouiCOM.Button Button0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private void Button0_PressedAfter(object sboObject, SAPbouiCOM.SBOItemEventArg pVal)
+        {
+            Payments outgoingPayment = (SAPbobsCOM.Payments)DiManager.Company.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oVendorPayments);
+            outgoingPayment.GetByKey(9);
+
+            DiManager.Recordset.DoQuery(DiManager.QueryHanaTransalte($"SELECT TransId FROM OVPM WHERE DocEntry = {9}"));
+
+            int jdtTransId = int.Parse(DiManager.Recordset.Fields.Item("TransId").Value.ToString());
+
+            JournalEntries journalEntry = (SAPbobsCOM.JournalEntries)DiManager.Company.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oJournalEntries);
+            journalEntry.GetByKey(jdtTransId);
+           var x1 = journalEntry.GetAsXML();
+            var x = outgoingPayment.Invoices.DistributionRule2;
+        
+        }
+    }
+}
---
+++ OutgoingPayment.b1f.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using SAPbobsCOM;
+using SAPbouiCOM;
+using SAPbouiCOM.Framework;
+using Application = SAPbouiCOM.Framework.Application;
+
+namespace CostCenterOutgoing
+{
+    [FormAttribute("426", "OutgoingPayment.b1f")]
+    class OutgoingPayment : SystemFormBase
+    {
+        public OutgoingPayment()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+            this.DataAddAfter += new DataAddAfterHandler(this.Form_DataAddAfter);
+
+        }
+
+        private void Form_DataAddAfter(ref SAPbouiCOM.BusinessObjectInfo pVal)
+        {
+            if (pVal.ActionSuccess)
+            {
+                int docEntry = 0;
+                try
+                {
+                    string xmlObjectKey = pVal.ObjectKey;
+                    XElement xmlnew = XElement.Parse(xmlObjectKey);
+                    XElement xElement = xmlnew.Element("DocEntry");
+                    if (xElement != null)
+                    {
+                        docEntry = int.Parse(xElement.Value);
+                        Payments outgoingPayment = (SAPbobsCOM.Payments)DiManager.Company.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oVendorPayments);
+                        outgoingPayment.GetByKey(docEntry);
+
+                        DiManager.Recordset.DoQuery(DiManager.QueryHanaTransalte($"SELECT TransId FROM OVPM WHERE DocEntry = {docEntry}"));
+
+                        int jdtTransId = int.Parse(DiManager.Recordset.Fields.Item("TransId").Value.ToString());
+
+                        JournalEntries journalEntry = (SAPbobsCOM.JournalEntries)DiManager.Company.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oJournalEntries);
+                        journalEntry.GetByKey(jdtTransId);
+
+                        outgoingPayment.Invoices.SetCurrentLine(0);
+                        var costCenter = outgoingPayment.Invoices.DistributionRule2;
+
+                        for (int i = 0; i < journalEntry.Lines.Count; i++)
+                        {
+                            journalEntry.Lines.SetCurrentLine(i);
+                            if (journalEntry.Lines.AccountCode == "1430")
+                            {
+                                journalEntry.Lines.CostingCode2 = costCenter;
+                            }
+                        }
+
+                       var xz =  journalEntry.Update();
+                    }
+                    else
+                    {
+                        Application.SBO_Application.SetStatusBarMessage("Outgoing Payment DocEntry Not Found",
+                            BoMessageTime.bmt_Short, true);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    // ignored
+                }
+            }
+
+        }
+
+        private void OnCustomInitialize()
+        {
+
+        }
+    }
+}
